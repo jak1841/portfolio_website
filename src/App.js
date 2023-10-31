2e1c3e1f3745f6b4d3cfb7abcd5b70ce035759c3
jak1841 <jaskarnd16@gmail.com>
2023-10-31 17:31:10 -0500

    Implemented the Education section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import UIC_LOGO from './images/UIC_LOGO.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const About_Me = () => {
+
+    return (
+        <div id="about_me_container">
+            <div>
+                <h2>Summary</h2>
+                <Summary/>
+            </div>
+            
+            <div>
+                <h2>My Education</h2>
+                <Education/>
+            </div>
+            
+        </div>
+    );
+
 }
 
-export default App;
+const Summary = () => {
+    return (
+        <div className='about_me_little_containers'>
+            
+        </div>
+    );
+}
+
+const Education = () => {
+    return(
+        <div className='about_me_little_containers'>
+            <img src={UIC_LOGO} alt="UIC LOGO" id='School_logo'></img>
+            <p1 id="university_name">University of Illinois at Chicago</p1>
+            <div id="School_info_container">
+                <p1 className="School_information">Bachelors of Science in Computer Science</p1>
+                <br></br>
+                <p1 className="School_information">GPA: 3.88/4.00</p1>
+                <br></br>
+                <p1 className="School_information">Deans List 2021, 2022, 2023</p1>
+                <br></br>
+                <p1 className="School_information">Expected Graduation: May 2024</p1>
+            </div>
+        </div>
+    );
+
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {About_Me} from './App.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Overall_structure = () => {
+  return (<div>
+    <h1>About Me</h1>
+    <About_Me/>
+
+    <h1>My Projects</h1>
+
+    <h1>My Experience</h1>
+
+    <h1>My Skills</h1>
+    
+
+  </div>);
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Overall_structure/>
+    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
